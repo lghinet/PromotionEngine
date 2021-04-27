fdb24297e73d4ab35205d7c89deedea8a17f8738
darktech <nick_20012002@yahoo.com>
2021-04-27 14:30:52 +0300

    adding models and an empty test

---
+++ src/PromotionEngine/CartModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class CartModel
+    {
+        public List<ItemModel> Items { set; get; }
+        public double TotalPrice { set; get; }
+
+    }
+
+    public class ItemModel
+    {
+        public string ItemCode { set; get; }
+        public double Quantity { set; get; }
+    }
+
+    public class ItemPriceModel
+    {
+        public string ItemCode { set; get; }
+        public double Price { set; get; }
+    }
+}
---
+++ tests/PromotionEngineTests/TestPromotionEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromotionEngineTests
+{
+    public class TestPromotionEngine
+    {
+        [Fact]
+        public void RunEngineWithNoPromotions()
+        {
+            
+        }
+    }
+}
