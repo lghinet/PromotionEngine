0280f8a588aa36765e22f70e7d31d0c114459f09
darktech <nick_20012002@yahoo.com>
2021-04-27 14:37:26 +0300

    creating first test

--- tests/PromotionEngineTests/TestPromotionEngine.cs
+++ tests/PromotionEngineTests/TestPromotionEngine.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PromotionEngine;
 using Xunit;
 
 namespace PromotionEngineTests
 {
     public class TestPromotionEngine
     {
+        private List<ItemPriceModel> _unitPrices = new()
+        {
+            new() {ItemCode = "A", Price = 50},
+            new() {ItemCode = "B", Price = 30},
+            new() {ItemCode = "C", Price = 20},
+            new() {ItemCode = "D", Price = 10},
+        };
+
         [Fact]
         public void RunEngineWithNoPromotions()
         {
-            
+            //arrange
+           
+
+            var items = new CartModel()
+            {
+
+              
+            }
+
+            //act 
+
+            // Assert
         }
     }
 }
